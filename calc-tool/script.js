8fd125ff5e40338d353b00866eaf02ea125d2742
言午 <2291767568@qq.com>
2026-09-10 09:22:12 +0800

    定义数据

---
+++ calc-tool/script.js
@@ -0,0 +1,10 @@
+const grades = [
+    { subject: "语文", score: Number(prompt("输入语文成绩")) },
+    { subject: "数学", score: Number(prompt("输入数学成绩")) },
+    { subject: "英语", score: Number(prompt("输入英语成绩")) },
+    { subject: "物理", score: Number(prompt("输入物理成绩")) },
+];
+
+// const cleanScores = (list) => list.filter(g => g.score >= 0 && g.score <= 100);
+
+// const 
